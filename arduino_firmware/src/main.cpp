6f9163faccc5b14a899ee799e7120fce49b82a6e
Oleksandr <kozoriz407@gmail.com>
2026-08-25 17:30:49 +0300

    IR remote start

--- arduino_firmware/src/main.cpp
+++ arduino_firmware/src/main.cpp
@@ -1,23 +1,27 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <IRremote.hpp>
 
-// Standard Arduino I2C pins (Uno/Nano):
-// Connect LCD SDA to A4
-// Connect LCD SCL to A5
-
-// Initialize the LCD address to 0x27 for a 16 chars and 2 line display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 const int alarmPin = 13;
-const int sensorPin = A0; // Pin for the ADC reading
+const int sensorPin = A0; 
+const int IRPin = 2;      
 const int ALARM_TIMEOUT = 250;
 
 bool alarmFlag = false;
-
+bool lastAlarmState = false; // Used to prevent LCD flickering
 int gasLevelValue = 0;
-
 const int GAS_ALERT_THRESHOLD = 600;
+
+// Variables for non-blocking alarm (replaces delay)
+unsigned long previousAlarmMillis = 0;
+bool alarmLedState = false;
+
+// Variables for non-blocking LCD updates
+unsigned long previousLcdMillis = 0;
+const int LCD_UPDATE_INTERVAL = 500; // Update LCD twice a second
 
 // Function declarations
 void start_alarm();
@@ -25,68 +29,105 @@ void stop_alarm();
 void check_alarm();
 void check_gas();
 void lcd_task();
+void check_ir();
 
-// Start an alarm
 void start_alarm() {
   alarmFlag = true;
 }
 
-// Stop the alarm
 void stop_alarm() {
   alarmFlag = false;
   digitalWrite(alarmPin, LOW);
+  alarmLedState = false;
 }
 
-// Buzz in case of alarm
+// Non-blocking alarm using millis() instead of delay()
 void check_alarm() {
   if (alarmFlag) {
-    digitalWrite(alarmPin, HIGH);
-    delay(ALARM_TIMEOUT);
-    digitalWrite(alarmPin, LOW);
-    delay(ALARM_TIMEOUT);
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousAlarmMillis >= ALARM_TIMEOUT) {
+      previousAlarmMillis = currentMillis;
+      // Toggle the alarm pin state
+      alarmLedState = !alarmLedState;
+      digitalWrite(alarmPin, alarmLedState ? HIGH : LOW);
+    }
   }
 }
 
 void check_gas() {
   if (gasLevelValue >= GAS_ALERT_THRESHOLD) {
     start_alarm();
-  }
-  else {
+  } else {
     stop_alarm();
   }
 }
 
 void lcd_task() {
-  if (!alarmFlag) {
-    // Display the data on the LCD
-    lcd.setCursor(0, 0);
-    lcd.print("Gas Level:");
-    
-    // Print the value on the second line
-    lcd.setCursor(0, 1);
-    lcd.print(gasLevelValue);
-    lcd.print("    "); // Clear leftover characters
-  } else {
+  // If the alarm state just changed, clear the screen once
+  if (alarmFlag != lastAlarmState) {
     lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("ALERT!!!");
-    lcd.setCursor(0, 1);
-    lcd.print("Gas level too high");
+    lastAlarmState = alarmFlag;
+  }
+
+  // Update LCD every 500ms to avoid freezing the Arduino
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousLcdMillis >= LCD_UPDATE_INTERVAL) {
+    previousLcdMillis = currentMillis;
+
+    if (!alarmFlag) {
+      lcd.setCursor(0, 0);
+      lcd.print("Gas Level:      ");
+      lcd.setCursor(0, 1);
+      lcd.print(gasLevelValue);
+      lcd.print("    "); 
+    } else {
+      lcd.setCursor(0, 0);
+      lcd.print("ALERT!!!        ");
+      lcd.setCursor(0, 1);
+      lcd.print("Gas level HIGH  ");
+    }
+  }
+}
+
+void check_ir() {
+  if (IrReceiver.decode()) {
+    // Grab the raw 32-bit data instead of the standard command
+    uint32_t rawValue = IrReceiver.decodedIRData.decodedRawData;
+    
+    // Ignore 0x0 values (which happen if you hold the button down too long)
+    if (rawValue != 0) {
+      Serial.print("Raw Hex for this button: 0x");
+      Serial.println(rawValue, HEX);
+
+      // --- HOW TO USE THIS ---
+      // 1. Press a button on your remote.
+      // 2. Look at the Serial Monitor to find its Raw Hex (e.g., 0xFFA25D).
+      // 3. Paste that hex code into an if-statement like the one below:
+
+      
+      if (rawValue == 0x5B1EDECC) {
+        Serial.println("You pressed Button 1!");
+        // Add your logic here
+      } 
+      else if (rawValue == ) { // Replace with another button's code
+        Serial.println("You pressed Button 2!");
+      }
+    }
+
+    IrReceiver.resume(); // Receive the next value
   }
 }
 
 void setup() {
   Serial.begin(9600); 
   pinMode(alarmPin, OUTPUT);
-  
-  // Initialize standard Hardware I2C (uses A4 and A5 automatically)
   Wire.begin();
 
-  // Initialize the LCD and turn on the backlight
   lcd.init();
   lcd.backlight();
   
-  // Print a startup test message
+  IrReceiver.begin(IRPin, ENABLE_LED_FEEDBACK); 
+
   lcd.setCursor(0, 0);
   lcd.print("Team 2 presents");
   delay(2000);
@@ -94,11 +135,16 @@ void setup() {
 }
 
 void loop() {
-  // Read the analog value
   gasLevelValue = analogRead(sensorPin);
 
-  // Handle the alarm
-  check_alarm();
+  check_ir();
   check_gas();
+  check_alarm();
   lcd_task();
 }
+
+/*
+TODO:
+remote accessible interface
+
+*/
