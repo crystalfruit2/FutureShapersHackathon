69bf95889534569591b92d95dd81dffeb835c3f9
Oleksandr <kozoriz407@gmail.com>
2026-08-25 19:41:17 +0300

    LCD setup

---
+++ pico_firmware/src/lcd.rs
@@ -0,0 +1,64 @@
+//! LCD control
+
+use embassy_rp::i2c::{I2c, Async};
+use embassy_rp::peripherals::I2C0;
+
+use embassy_time::{Delay, Timer};
+
+use hd44780_driver::{Cursor, CursorBlink, HD44780};
+use hd44780_driver::bus::I2CBus;
+
+/// i2c address of the display
+const ADDR: u8 = 0x27;
+const EMPTY_LINE: &str = "                "; // 16 spaces for clearing the line
+
+#[derive(Clone, Copy)]
+enum DisplayLine {
+    Top,
+    Bottom,
+}
+
+async fn clean_line(
+    line: DisplayLine,
+    lcd: &mut HD44780<I2CBus<I2c<'static, I2C0, Async>>>,
+    delay: &mut Delay
+) {
+    match line {
+        DisplayLine::Top => lcd.set_cursor_pos(0, delay).unwrap(), // move to the first line
+        DisplayLine::Bottom => lcd.set_cursor_pos(40, delay).unwrap(), // move to the second line
+    }
+    lcd.write_str(EMPTY_LINE, delay).unwrap();
+}
+
+async fn write_line(
+    line: DisplayLine,
+    text: &str,
+    lcd: &mut HD44780<I2CBus<I2c<'static, I2C0, Async>>>,
+    delay: &mut Delay
+) {
+    clean_line(line, lcd, delay).await;
+    match line {
+        DisplayLine::Top => lcd.set_cursor_pos(0, delay).unwrap(), // move to the first line
+        DisplayLine::Bottom => lcd.set_cursor_pos(40, delay).unwrap(), // move to the second line
+    }
+    lcd.write_str(text, delay).unwrap();
+}
+
+#[embassy_executor::task]
+pub async fn display_task(i2c: I2c<'static, I2C0, Async>) {
+    // initial setup
+    let mut delay = Delay;
+    let mut lcd = HD44780::new_i2c(i2c, ADDR, &mut delay).unwrap();
+
+    // clear the previous state
+    lcd.reset(&mut delay).unwrap();
+    lcd.clear(&mut delay).unwrap();
+
+    // turn off the blinking cursor
+    lcd.set_cursor_blink(CursorBlink::Off, &mut delay).unwrap();
+    lcd.set_cursor_visibility(Cursor::Invisible, &mut delay).unwrap();
+
+    lcd.write_str("Welcome!", &mut delay).unwrap();
+    Timer::after_secs(2).await;
+    lcd.clear(&mut delay).unwrap();
+}
--- pico_firmware/src/main.rs
+++ pico_firmware/src/main.rs
@@ -2,14 +2,23 @@
 #![no_main]
 
 mod state;
+mod lcd;
 
 use defmt_rtt as _;
 use panic_probe as _;
 
 use embassy_executor::Spawner;
+
+use embassy_rp::bind_interrupts;
 use embassy_rp::gpio::{Level, Output};
+use embassy_rp::i2c::{Config, I2c, InterruptHandler};
+use embassy_rp::peripherals;
 
 use defmt::info;
+
+bind_interrupts!(struct Irqs {
+    I2C0_IRQ => InterruptHandler<peripherals::I2C0>;
+});
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -25,4 +34,10 @@ async fn main(spawner: Spawner) {
     let green = Output::new(p.PIN_4, Level::Low);
     let blue = Output::new(p.PIN_5, Level::Low);
     spawner.spawn(state::rgb_status(red, green, blue).unwrap());
+
+    // LCD setup
+    let sda = p.PIN_16;
+    let scl = p.PIN_17;
+    let i2c = I2c::new_async(p.I2C0, scl, sda, Irqs, Config::default());
+    spawner.spawn(lcd::display_task(i2c).unwrap());
 }
