6350ed08553549801425b8da04a1c6321a8e6b62
Oleksandr <kozoriz407@gmail.com>
2026-08-25 19:07:52 +0300

    Init RGB

--- pico_firmware/src/main.rs
+++ pico_firmware/src/main.rs
@@ -16,6 +16,13 @@ async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
     info!("Hello world!");
 
+    // Buzzer alert setup
     let buzzer = Output::new(p.PIN_2, Level::Low);
     spawner.spawn(state::buzzer_alert(buzzer).unwrap());
+
+    // Status LED setup
+    let red = Output::new(p.PIN_3, Level::Low);
+    let green = Output::new(p.PIN_4, Level::Low);
+    let blue = Output::new(p.PIN_5, Level::Low);
+    spawner.spawn(state::rgb_status(red, green, blue).unwrap());
 }
